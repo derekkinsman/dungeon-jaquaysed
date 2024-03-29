bd51ae2b680623dc44fa51ed14f8b19e71724a74
Derek J. Kinsman <derek@unttld.com>
2024-03-29 10:26:57 -0400

    Updating.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,47 @@
 import React from "react";
 import { Metadata } from 'next';
+import page from '@styles/components/Page.module.scss';
 
 export const metadata: Metadata = {
   title: 'Jaquaysing the Dungeon',
-  description: 'Matter of Factly.'
+  description: "It's 'Jaquaysing' and only 'Jaquaysing'.",
+
+  openGraph: {
+    title: 'Jaquaysing the Dungeon',
+    description: "It's 'Jaquaysing' and only 'Jaquaysing'.",
+    url: 'https://jaquaysingthedungeon.com/',
+    siteName: 'Jaquaysing the Dungeon',
+    images: [
+      {
+        url: 'https://xanderingthedungeon.com/card.png', // Must be an absolute URL
+        width: 1280,
+        height: 628,
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Jaquaysing the Dungeon',
+    description: "It's 'Jaquaysing' and only 'Jaquaysing'.",
+    images: ['https://jaquaysingthedungeon.com/card.png'], // Must be an absolute URL
+  },
 }
 
 export default function Page() {
-  return <>
-    <h1>Jaquaysing [juh-kwey-zing]</h1>
-    <p>verb</p>
-    <ol>
-      <li>to design non-linear dungeons for fantasy games of role-play.</li>
-    </ol>
-    <p>Words that May Be Confused with Jaquaysing</p>
-    <p><a href='https://xanderingthedungeon.com/'>Xandering</a></p>
-  </>
+  return <section className={`${page.section}`}>
+    <section className={`${page.word}`}>
+      <h1 className={`${page.title}`}><span>Jaquaysing</span> [juh-kwey-zing]</h1>
+      <p className={`${page.type}`}>verb</p>
+      <ol className={`${page.definitions}`}>
+        <li className={`${page.definition}`}>to design non-linear dungeons for fantasy games of role-play.</li>
+      </ol>
+    </section>
+    <section className={`${page.additional}`}>
+      <p className={`${page.confusion}`}>Words that May Be Confused with Jaquaysing:</p>
+      <p className={`${page.wordlist}`}><a href='https://xanderingthedungeon.com/'>Xandering</a></p>
+    </section>
+  </section>
 }
